20776748ac9908176b94cef9dc2c6d74c7dbac9c
Daniel Harris <09harrisda@gmail.com>
2018-07-10 10:51:26 -0700

    This commit will add a user flow for retrieving an image in the UITabBarController

---
+++ Makestagram/Controllers/MainTabBarController.swift
@@ -0,0 +1,43 @@
+//
+//  MainTabBarController.swift
+//  Makestagram
+//
+//  Created by Daniel Harris on 09/07/2018.
+//  Copyright © 2018 Daniel Harris. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class MainTabBarController: UITabBarController {
+    
+    //Properties
+    let photoHelper = MGPhotoHelper()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        photoHelper.completionHandler = { image in
+            print("handle image")
+        }
+        
+        delegate = self
+        tabBar.unselectedItemTintColor = .black
+    }
+    
+    
+    
+}
+
+extension MainTabBarController: UITabBarControllerDelegate {
+    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+        if viewController.tabBarItem.tag == 1 {
+            photoHelper.presentActionSheet(from: self)
+            return false
+        }
+        else {
+            return true
+        }
+    }
+}
